372c20349c2b0c4a35a9272567e1c577f26af6d5
VinoSubbu <vinothkumarsubburaman@gmail.com>
2024-07-20 11:03:03 +0530

    'link-initiated'

--- src/components/NavigationMenu.jsx
+++ src/components/NavigationMenu.jsx
@@ -1,29 +1,43 @@
-import React from 'react';
-import '../assets/styles/style.css'
+import React from "react";
+import "../assets/styles/style.css";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const NavigationMenu = () => {
+  const navigate = useNavigate();
+
+  const handleAllCourse = () => {
+    navigate('/');
+  }
+
+  const handleFullStackDevelopment = () => {
+    navigate('/full-stack-development');
+  }
+
+  const handleDataScience = () => {
+    navigate('/data-science');
+  }
+
+  const handleCyberSecurity = () => {
+    navigate('/cyber-security');
+  }
+
+  const handleCareer = () => {
+    navigate('/career');
+  }
+
+
   return (
-    <nav className='navbar'>
-      <div className='heading-div'>
-      <h1 className='heading'>React Router Task</h1>
+    <nav className="navbar">
+      <div className="heading-div">
+        <h1 className="heading">React Router Task</h1>
       </div>
-      <ul className='navbar-ul'>
-        <li>
-          <a href="/">All Courses</a>
-        </li>
-        <li>
-          <a href="/full-stack-development">Full Stack Development</a>
-        </li>
-        <li>
-          <a href="/data-science">Data Science</a>
-        </li>
-        <li>
-          <a href="/cyber-security">Cyber Security</a>
-        </li>
-        <li>
-          <a href="/career">Career</a>
-        </li>
-      </ul>
+      <div className="navbar-ul">
+      <NavLink className="navlink" to="/" exact>All Courses</NavLink>
+        <NavLink className="navlink" to="/full-stack-development">Full Stack Development</NavLink>
+        <NavLink className="navlink" to="/data-science">Data Science</NavLink>
+        <NavLink className="navlink" to="/cyber-security">Cyber Security</NavLink>
+        <NavLink className="navlink" to="/career">Career</NavLink>
+      </div>
     </nav>
   );
 };
